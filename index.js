a8719608373ae6e2161d8b58678cb035695ebaf7
RohitoOo <bhambhani.ba@gmail.com>
2018-05-28 15:20:03 +0530

    Balanced Parentheses

---
+++ index.js
@@ -0,0 +1,17 @@
+function balanced(string) {
+
+  return !string.split("").reduce(function(previous,char){
+
+
+if(char === "("){
+  return ++previous;
+}
+if (char === ")"){
+  return --previous;
+}
+
+}, 0);
+
+}
+
+balanced("()" )
